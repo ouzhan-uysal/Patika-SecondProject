4aadb0084d6d55546f8bf1770c79d572fb4d4ab1
Oğuzhan <ouzhan.ysl@gmail.com>
2021-09-24 11:15:36 +0300

    Update animals.js

--- src/animals.js
+++ src/animals.js
@@ -37,13 +37,23 @@ export const instanceData = [];
 
 const dog1 = new Dog('Ralph Wolf', 'https://via.placeholder.com/256x128.png?text=Ralph+Wolf', 4, 5);
 const dog2 = new Dog('Sam Sheepdog', 'https://via.placeholder.com/256x128.png?text=Sam+Sheepdog', 4, 7);
+const dog3 = new Dog('x3', 'https://via.placeholder.com/256x128.png?text=x3', 4, 7);
+const dog4 = new Dog('x4', 'https://via.placeholder.com/256x128.png?text=x4', 4, 7);
+
 const duck1 = new Duck('Daffy Duck', 'https://via.placeholder.com/256x128.png?text=Daffy+Duck', 2, 3);
 const duck2 = new Duck('Daisy Duck', 'https://via.placeholder.com/256x128.png?text=Daisy+Duck', 2, 6);
+const duck3 = new Duck('x5', 'https://via.placeholder.com/256x128.png?text=x5', 2, 3);
+const duck4 = new Duck('x6', 'https://via.placeholder.com/256x128.png?text=x6', 2, 3);
+
 
 instanceData.push(dog1);
 instanceData.push(dog2);
+instanceData.push(dog3);
+instanceData.push(dog4);
 instanceData.push(duck1);
 instanceData.push(duck2);
+instanceData.push(duck3);
+instanceData.push(duck4);
 
 // dog1.setAnimalName("murtaza");
 // console.log(dog1.getAnimalInfo());
